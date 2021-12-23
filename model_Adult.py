a6797af0b2b782691fb1d6e097385c21fae0b449
LucStk <luc.sterkers@laposte.net>
2021-12-23 17:29:16 +0100

    "chargement données Adults"

--- model_Adult.py
+++ model_Adult.py
@@ -2,10 +2,13 @@ import torch
 import torchvision
 import pandas
 from sklearn.preprocessing import LabelBinarizer
+import numpy as np
 
 """
-Télécharger le dataset csv "Adult income dataset" sur kaggle 
-https://www.kaggle.com/wenruliu/adult-income-dataset
+    Télécharger le dataset csv "Adult income dataset" sur kaggle 
+    https://www.kaggle.com/wenruliu/adult-income-dataset
+
+    Description de la base : http://www.cs.toronto.edu/~delve/data/adult/adultDetail.html
 """
 
 data = pandas.read_csv("adult.csv")
@@ -18,11 +21,31 @@ Les paramètres :
 
 On prédit le genre
 
+Sont continues:
+    'age', 'fnlwgt', 'educational-num',
+    'capital-gain', 'capital-loss', 'hours-per-week'
+
+On encode one-hot : 
+    workclass, relationship, race, native-country, occupation, marital-status
+
+On binarise :
+    income et gender
+
+On n'utilise pas "education" mais sa version en continue avec educational-num
 """
 y = torch.Tensor(LabelBinarizer().fit_transform(data.gender)).squeeze()
 
+data_continues = data[['age', 'fnlwgt', 'educational-num',
+                       'capital-gain', 'capital-loss', 'hours-per-week']].to_numpy()
+
+data_one_hot = pandas.get_dummies(data[['workclass', 'relationship', 'race', 'native-country', 'occupation', 'marital-status']]).to_numpy()
+data_binary = LabelBinarizer().fit_transform(data.income)
+dataset = np.concatenate((data_continues, data_binary, data_one_hot), axis = 1)
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = 10, shuffle = True)
 
 
+### Création du modèle
 
 
 print()
