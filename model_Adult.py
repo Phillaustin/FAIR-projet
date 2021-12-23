6ce6e727805c238618df9e6b7a4be8e0649715d7
LucStk <luc.sterkers@laposte.net>
2021-12-23 17:50:15 +0100

    définition model

--- model_Adult.py
+++ model_Adult.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torchvision
 import pandas
 from sklearn.preprocessing import LabelBinarizer
@@ -39,13 +40,38 @@ data_continues = data[['age', 'fnlwgt', 'educational-num',
                        'capital-gain', 'capital-loss', 'hours-per-week']].to_numpy()
 
 data_one_hot = pandas.get_dummies(data[['workclass', 'relationship', 'race', 'native-country', 'occupation', 'marital-status']]).to_numpy()
-data_binary = LabelBinarizer().fit_transform(data.income)
-dataset = np.concatenate((data_continues, data_binary, data_one_hot), axis = 1)
-
-dataloader = torch.utils.data.DataLoader(dataset, batch_size = 10, shuffle = True)
-
-
-### Création du modèle
+data_binary  = LabelBinarizer().fit_transform(data.income)
+dataset      = np.concatenate((data_continues, data_binary, data_one_hot), axis = 1)
+dataloader   = torch.utils.data.DataLoader(dataset, batch_size = 10, shuffle = True)
 
 
-print()
+INPUT_SIZE  = dataset.shape[1]
+OUTPUT_SIZE = y.shape[1]
+"""
+    Création du Séléctionneur, 
+    on met qu'une seule couche pour l'instant et on sort en sigmoid pour avoir des proba
+"""
+H1_SIZE = 100
+
+selector = nn.Sequential(
+    nn.Linear(INPUT_SIZE, H1_SIZE)   , nn.ReLU(),
+    nn.Linear(H1_SIZE   , INPUT_SIZE), nn.Sigmoid()
+)
+
+"""
+    Création du Prédicteur
+"""
+H1_SIZE = 50
+H2_SIZE = 30
+predicteur = nn.Sequential(
+    nn.Linear(INPUT_SIZE, H1_SIZE)    , nn.ReLU(),
+    nn.Linear(H1_SIZE   , H2_SIZE)    , nn.ReLU(),
+    nn.Linear(H2_SIZE   , OUTPUT_SIZE), nn.Sigmoid()
+)
+
+"""
+Optimisation
+"""
+NB_ITERATION = 100
+for i in range(NB_ITERATION):
+    for b in 
